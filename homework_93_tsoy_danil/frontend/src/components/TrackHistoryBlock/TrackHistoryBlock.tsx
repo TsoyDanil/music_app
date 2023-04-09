f34140098950d88c8ede66cc18dbeb3685988f75
danil tsoy <abdullaevdanil2002@gmail.com>
2023-04-09 19:58:25 +0600

    Fixxed track history where it breaks after some data was deleted

--- homework_93_tsoy_danil/frontend/src/components/TrackHistoryBlock/TrackHistoryBlock.tsx
+++ homework_93_tsoy_danil/frontend/src/components/TrackHistoryBlock/TrackHistoryBlock.tsx
@@ -2,12 +2,12 @@ import ITrackHistoryBlockProps from "./ITrackHistoryBlockProps";
 import styles from './TrackHistoryBlock.module.css'
 
 const TrackHistoryBlock: React.FunctionComponent<ITrackHistoryBlockProps> = (props): React.ReactElement => {
-
+    
     return(
         <div className={styles.TrackHistoryBlock}>
-            <p>{props.trackHistory.track.title}</p>
-            <p>{props.trackHistory.track.album.artist.name}</p>
-            <p>{new Date(props.trackHistory.datetime).toLocaleString()}</p>
+            <p>{props.trackHistory?.track?.title !== undefined ? props.trackHistory?.track?.title : 'Track data was deleted'}</p>
+            <p>{props.trackHistory?.track?.album?.artist?.name !== undefined ? props.trackHistory.track.album.artist.name :'Track\'s album was deleted'}</p>
+            <p>{new Date(props.trackHistory?.datetime).toLocaleString()}</p>
         </div>
     )
 }
