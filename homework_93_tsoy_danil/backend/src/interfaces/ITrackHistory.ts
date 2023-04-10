d54ac602a2e6e40b84206896ccf0d7a9f127ebc8
danil tsoy <abdullaevdanil2002@gmail.com>
2023-04-10 20:12:25 +0600

    Changed tracthistory inteface to it been able to get _id

--- homework_93_tsoy_danil/backend/src/interfaces/ITrackHistory.ts
+++ homework_93_tsoy_danil/backend/src/interfaces/ITrackHistory.ts
@@ -1,7 +1,9 @@
+import { Types } from "mongoose";
 import ITrack from "./ITrack";
 import IUser from "./IUser";
 
 export default interface ITrackHistory {
+    _id: Types.ObjectId
     user: IUser['_id']
     track: ITrack
     datetime: Date
